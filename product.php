b7a64c037ff9d905c8187ba180186be787037ded
ytk32 <gangbenyongxi55@gmail.com>
2024-10-01 11:50:41 +0900

    uof

--- product.php
+++ product.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="common/css/reset.css">
     <link rel="stylesheet" href="common/css/common.css">
+    <link rel="stylesheet" href="common/css/product.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
@@ -22,36 +23,51 @@
     <!-- コンテンツ -->
     <main>
 
-        <body>
-            <!-- test -->
-            <!--  -->
-            <p>pppheed</p>
+
+        <!-- test -->
+        <!--             
             <form action="product.php" method="post">
                 <section class="product_hero">
-                    <h1 class="product_title">商品一覧</h1>
-                    <?php
-                    //データベース接続
-                    $pdo = new PDO('mysql:host=localhost;dbname=donuts;charset=utf8', 'donuts', 'password');
-                    // SQL文の準備
-                    $sql = $pdo->prepare('select * from product where id=?');
-                    // SQL文の実行
-                    foreach ($pdo->query('select * from product') as $row) {
-                        echo '<p>';
-                        echo $row['id'];
-                        echo $row['name'];
-                        echo $row['price'];
-                        echo '</p>';
-                    }
-                    //HTMLの生成・出力
+                    <h1 class="product_title">商品一覧</h1> -->
 
-                    //                     echo <<<END
-                    //                     <p>$row[id]</p>
+        <?php
+        //データベース接続
+        $pdo = new PDO('mysql:host=localhost;dbname=donuts;charset=utf8', 'donuts', 'password');
+        // SQL文の準備
+        $sql = $pdo->prepare('select * from product where id=?');
+        // SQL文の実行
+        foreach ($pdo->query('select * from product') as $row) {
+            // echo '<p>';
+            // echo $row['name'];
+            // echo $row['price'];
+            // echo '</p>';
+            echo <<<END
+                        <form action="cart.php" method="post">                        <p><img alt="image" src="common/images/{$row['id']}.png"></p>
+                        <p class="product_submenu"><a href="detail.php">{$row['name']}</a></p>
+                         <p>{$row['price']}</p>
+                        END;
 
-                    // END;
-                    ?>
-                </section>
-            </form>
-            <!-- <div class="product_content">
+            // ヒアドキュメント2つ目
+            echo <<<END
+                        <input type="hidden" name="id" value="{$row['id']}">
+                        <input type="hidden" name="name" value="{$row['name']}">
+                        <input type="hidden" name="price" value="{$row['price']}">
+                        <p><input type="submit" class="product_submit" value="カートに追加"></p>
+                        </form>
+                        END;
+        }
+        //HTMLの生成・出力
+
+        //                     echo <<<END
+        //                     <p>$row[id]</p>
+
+        // END;
+        ?>
+
+        <!-- </section>
+        </form> -->
+
+        <!-- <div class="product_content">
                 <div class="product_menu">
                     <div class="product_img"><a href="detail.php"><img src="common/images/products-CCoriginal.png" alt="CCdonuts"></a></div>
                     <p class="product_submenu"><a href="detail.php">CCドーナツ 当店オリジナル （5個入り）</a></p>
