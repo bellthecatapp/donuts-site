8e4252ccf3064c6b503768790b38a630bd2c9c6c
ytk32 <gangbenyongxi55@gmail.com>
2024-10-04 11:31:56 +0900

    login

--- login-complete.php
+++ login-complete.php
@@ -26,38 +26,34 @@
     <!-- ユーザー名 -->
 
     <!-- コンテンツ -->
-    <?php
-    //既存のセッション破棄
-    unset($_SESSION['customer']);
-    //データベース接続
-    require 'includes/database.php';
-    //SQL文の準備
-    $sql = $pdo->prepare('select * customer where mail=? and password=?');
-    // SQL文の実行
-    $sql->execute([
-        $_REQUEST['login'],
-        $_REQUEST['password']
-    ]);
-    // 取得したデータをセッションのcustomer変数に保存
-    foreach ($sql as $row) {
-        $_SESSION['customer'] = [
-            'id' => $row['id'],
-            'name' => $row['name'],
-            'address' => $row['address'],
-            'mail' => $row['mail'],
-            'password' => $row['password']
-        ];
-    }
-    if (isset($_SESSION['customer'])) {
-        echo <<<END
-
-
-            END;
-    } else {
-        echo 'ログイン名またはパスワードが違います';
-    }
-    ?>
     <main>
+
+        <?php
+        //既存のセッション破棄
+        unset($_SESSION['customer']);
+        //データベース接続
+        require 'includes/database.php';
+        //SQL文の準備
+        $sql = $pdo->prepare('select * from customer where mail=? and password=?');
+        // SQL文の実行
+        $sql->execute([
+            $_REQUEST['login'],
+            $_REQUEST['password']
+        ]);
+        // 取得したデータをセッションのcustomer変数に保存
+        foreach ($sql as $row) {
+            $_SESSION['customer'] = [
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'kana' => $row['kana'],
+                'post_code' => $row['post_code'],
+                'address' => $row['address'],
+                'mail' => $row['mail'],
+                'password' => $row['password']
+            ];
+        }
+        if (isset($_SESSION['customer'])) {
+            echo <<<END
         <div class="inner">
             <h1 class="common_subpage">ログイン完了</h1>
             <div class="login_wrapper">
@@ -71,6 +67,31 @@
             </div>
         </div>
 
+
+
+
+END;
+        } else {
+            echo <<<END
+            <div class="inner">
+            <h1 class="common_subpage">ログイン失敗</h1>
+            <div class="login_wrapper">
+                <div class="login_frame_complete">
+                    <p class="complete_text">emailまたはパスワードが違います</p>
+                </div>
+                <div class="login_frame2">
+                    <p class="linkedtext"><a href="index.php">TOPページへ戻る</a></p>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+END;
+        }
+        ?>
     </main>
 
     <?php
