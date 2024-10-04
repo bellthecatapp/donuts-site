5dcb1361935b46f6e6af00f91b15550c8a96fdce
bellthecatapp <bellthecatapp@gmail.com>
2024-10-04 10:33:22 +0900

    login logout html css fixed

--- logout-input.php
+++ logout-input.php
@@ -9,20 +9,35 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="common/css/login_out.css">
     <!-- ↑ページ別の.cssをリンク -->
     <title>ログアウト｜C.C.Donuts</title>
 </head>
 
 <body>
+    <?php
+    require 'includes/header.php';
+    ?>
     <!-- パンくず -->
 
     <!-- ユーザー名 -->
 
     <!-- コンテンツ -->
     <main>
+        <div class="inner">
+            <h1 class="common_subpage">ログアウト</h1>
+            <div class="login_wrapper">
+                <div class="login_frame">
+                    <p class="logout">ログアウトしますか？</p>
+                    <form action="login-complete.php" method="post">
+                        <input type="submit" value="ログアウトする" class="common_btn_lg">
+                    </form>
+                </div>
+                <div class="login_frame2"> </div>
+            </div>
+        </div>
 
     </main>
-</body>
-
-</html>
+    <?php
+    require 'includes/footer.php';
+    ?>
